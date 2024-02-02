89534898f6435c3c67d7070c9b1dd0f88138e0f6
millemaliu <millemal@iu.edu>
2024-02-02 10:49:46 -0500

    Add files via upload

---
+++ src/main/java/edu/iu/c322/test1/Test1Application.java
@@ -0,0 +1,16 @@
+package edu.iu.c322.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootConfiguration
+public class Test1Application {
+	public Test1Application() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1Application.class, args);
+	}
+
+}
---
+++ src/main/java/edu/iu/c322/test1/controllers/QuizController.java
@@ -0,0 +1,7 @@
+package edu.iu.c322.test1.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizController {
+}
